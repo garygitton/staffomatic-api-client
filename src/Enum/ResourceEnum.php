31a2a1f5a6689e1f7ade9dd79e4523369a7fb484
Gary Gitton <gary.gitton@gmail.com>
2018-11-20 15:28:24 +0100

    list resources

---
+++ src/Enum/ResourceEnum.php
@@ -0,0 +1,22 @@
+<?php
+namespace Staffomatic\Api\Client\Enum;
+
+class ResourceEnum
+{
+    const ABSENCE_TYPE = 'absence type';
+    const ABSENCE = 'absence';
+    const ACCOUNT = 'account';
+    const APPLICATION = 'application';
+    const BREAK_TIMER = 'break timer';
+    const DEPARTMENT = 'department';
+    const EVENT = 'event';
+    const FORM_FIELD = 'form field';
+    const LOCATION = 'location';
+    const NEWS_ITEM = 'news item';
+    const NOTIFICATION_SETTING = 'notification setting';
+    const SCHEDULE = 'schedule';
+    const SHIFT = 'shift';
+    const SPECIAL_DAY = 'special day';
+    const USER = 'user';
+    const WORK_TIMER = 'work timer';
+}
